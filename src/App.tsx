6df49e26d5e60e62a8e4a04b8a7e78272f26fb58
Vitorhpx <vitorhugoperles@gmail.com>
2019-11-24 19:07:57 -0300

    Adds login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import Header from './Components/mol.header/header.component';
-import GlobalThemeProvider from './Components/obj.theme-provider/global-theme-provider.component';
-import MainPage from './Pages/Main.page';
-import { SnackbarProvider } from 'notistack';
-import { Product } from './Components/mol.product-modal/product-modal.component';
-import ProductCheckout from './Pages/Product-checkout.page';
-import Thanks from './Pages/thanks.page';
+import React from "react";
+import Header from "./Components/mol.header/header.component";
+import GlobalThemeProvider from "./Components/obj.theme-provider/global-theme-provider.component";
+import MainPage from "./Pages/Main.page";
+import { SnackbarProvider } from "notistack";
+import { Product } from "./Components/mol.product-modal/product-modal.component";
+import ProductCheckout from "./Pages/Product-checkout.page";
+import Thanks from "./Pages/thanks.page";
 
-import PlacesSelection, { Place } from './Pages/place-selection.page';
+import PlacesSelection, { Place } from "./Pages/place-selection.page";
+import SignIn from "./Pages/Login.page";
 
+// MOCK PAGES STATE
 enum Pages {
-  main = 'main',
-  checkout = 'checkout',
-  places = 'places',
-  thanks = 'thanks',
+  login = "login",
+  main = "main",
+  checkout = "checkout",
+  places = "places",
+  thanks = "thanks"
 }
 
 const App = () => {
   const [products, setProducts] = React.useState<Product[]>([]);
-  const [activePage, setActivePage] = React.useState<Pages>(Pages.main);
+  const [activePage, setActivePage] = React.useState<Pages>(Pages.login);
   const handleCheckout = () => {
     setActivePage(Pages.thanks);
     setProducts([]);
@@ -27,7 +30,16 @@ const App = () => {
   return (
     <GlobalThemeProvider>
       <SnackbarProvider maxSnack={1}>
-        <Header onBackClick={getBackClick(activePage, setActivePage)} text={getHeaderText(activePage)} />
+        {activePage !== Pages.login && (
+          <Header
+            onBackClick={getBackClick(activePage, setActivePage)}
+            text={getHeaderText(activePage)}
+          />
+        )}
+
+        {activePage === Pages.login && (
+          <SignIn onLogin={() => setActivePage(Pages.main)} />
+        )}
         {activePage === Pages.main && (
           <MainPage
             onAddProduct={product => setProducts([...products, product])}
@@ -36,10 +48,17 @@ const App = () => {
           />
         )}
         {activePage === Pages.checkout && (
-          <ProductCheckout products={products} onNextClick={() => setActivePage(Pages.places)} />
+          <ProductCheckout
+            products={products}
+            onNextClick={() => setActivePage(Pages.places)}
+          />
         )}
-        {activePage === Pages.places && <PlacesSelection places={placesMock} onNextClick={handleCheckout} />}
-        {activePage === Pages.thanks && <Thanks onNextClick={() => setActivePage(Pages.main)} />}
+        {activePage === Pages.places && (
+          <PlacesSelection places={placesMock} onNextClick={handleCheckout} />
+        )}
+        {activePage === Pages.thanks && (
+          <Thanks onNextClick={() => setActivePage(Pages.main)} />
+        )}
       </SnackbarProvider>
     </GlobalThemeProvider>
   );
@@ -48,15 +67,15 @@ const App = () => {
 const getHeaderText = (page: Pages) => {
   switch (page) {
     case Pages.main:
-      return 'Escaneie um produto';
+      return "Escaneie um produto";
 
     case Pages.checkout:
-      return 'Seus Recicláveis';
+      return "Seus Recicláveis";
 
     case Pages.places:
-      return 'Escolha um local de coleta';
+      return "Escolha um local de coleta";
     default:
-      return 'Código Verde';
+      return "Código Verde";
   }
 };
 
@@ -76,26 +95,26 @@ const getBackClick = (page: Pages, setActivePage: any) => {
 };
 const placesMock: Place[] = [
   {
-    name: 'Cooperativa Catadores',
-    address: `R. Lorena, 20 - Santo Antônio`,
+    name: "Cooperativa Catadores",
+    address: `R. Lorena, 20 - Santo Antônio`
   },
   {
-    name: 'Reciclagem e Cia ',
-    address: `R. Frederica Saciloti, 346 - Vila Nunes`,
+    name: "Reciclagem e Cia ",
+    address: `R. Frederica Saciloti, 346 - Vila Nunes`
   },
   {
-    name: 'Recicla Tudo',
-    address: `R. São Benedito, 123 - Vila Nunes`,
+    name: "Recicla Tudo",
+    address: `R. São Benedito, 123 - Vila Nunes`
   },
 
   {
-    name: 'AP Reciclagem de Plásticos',
-    address: `R. Rio Grande do Norte, 291 - Cidade Industrial`,
+    name: "AP Reciclagem de Plásticos",
+    address: `R. Rio Grande do Norte, 291 - Cidade Industrial`
   },
   {
-    name: 'Ferro Velho do Joãozinho',
-    address: `R. Abadias José Correa, 259 - Vila Sta Edwiges`,
-  },
+    name: "Ferro Velho do Joãozinho",
+    address: `R. Abadias José Correa, 259 - Vila Sta Edwiges`
+  }
 ];
 
 export default App;
---
+++ src/Pages/Login.page.tsx
@@ -0,0 +1,127 @@
+import React, { FormEvent } from "react";
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import SvgLogo from "../Components/atm.icons/logo";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles(theme => ({
+  "@global": {
+    body: {
+      backgroundColor: theme.palette.common.white
+    }
+  },
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center"
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1)
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2)
+  }
+}));
+
+interface SignInProps {
+  onLogin: () => void;
+}
+const SignIn: React.FunctionComponent<SignInProps> = props => {
+  const classes = useStyles();
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    props.onLogin();
+  };
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <SvgLogo />
+        <form className={classes.form} noValidate onSubmit={handleSubmit}>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Insira seu e-mail"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Insira sua senha"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Permanecer conectado"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Entrar
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Esqueceu sua senha?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Não possui uma conta?"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+  );
+};
+
+export default SignIn;
